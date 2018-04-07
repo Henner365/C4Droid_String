074e3eb9b8a93993c22ec990406cbd723383b6bd
Henner365 <lenovo@arboesaidy.com>
2018-04-07 18:34:19 +0200

    commit first

---
+++ first.h
@@ -0,0 +1,34 @@
+
+#ifndef  first_h
+#define  first_h
+
+#include <iostream>
+#include <stdio.h>
+
+typedef unsigned int str_int;
+
+
+// string kan blive så sto-. > 16 tegn.- den ern�dt til at være å
+// paa heapen. 
+
+class string{
+	//protected:
+	friend std::ostream& operator<<(std::ostream& os, string& prnt);
+	private:
+	//char stackstore[16];	
+	char*   pstrA;
+	str_int length;
+	//int& ir;
+	public:
+	char* get();
+	~string ();
+	string();
+	string(const char * cchp);
+	string( string& tocopy);//copyconstructor
+	string& operator=( string& astr);//assignment operator
+	str_int get_length(const char* ccp);
+
+};
+
+
+#endif 
